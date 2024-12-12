614fc4deb85d557ccdea1c80ed033e0469e88e5e
Hieu <21520235@gm.uit.edu.vn>
2024-12-13 00:08:07 +0700

    update arrow

--- Uber/src/navigation/CustomDrawer.js
+++ Uber/src/navigation/CustomDrawer.js
@@ -23,8 +23,8 @@ const CustomDrawer = (props) => {
           }}/>
 
           <View>
-            <Text style={{color: 'white', fontSize: 24}}>Vadim Savin</Text>
-            <Text style={{color: 'lightgrey'}}>5.00 *</Text>
+            <Text style={{color: 'white', fontSize: 24}}>Tên Người dùng</Text>
+            <Text style={{color: 'lightgrey'}}>(đánh giá sao tài xếxế)</Text>
           </View>
         </View>
 
@@ -39,19 +39,19 @@ const CustomDrawer = (props) => {
         }}>
           <Pressable
             onPress={() => {console.warn('Messages')}}>
-            <Text style={{color: '#dddddd', paddingVertical: 5,}}>Messages</Text>
+            <Text style={{color: '#dddddd', paddingVertical: 5,}}>Tin nhắn</Text>
           </Pressable>
         </View>
 
         { /* Do more */}
         <Pressable
           onPress={() => {console.warn('Make Money Driving')}}>
-          <Text style={{color: '#dddddd', paddingVertical: 5,}}>Do more with your account</Text>
+          <Text style={{color: '#dddddd', paddingVertical: 5,}}>Làm được nhiều hơn với tài khoản của bạn</Text>
         </Pressable>
 
         {/* Make money */}
         <Pressable onPress={() => {console.warn('Make Money Driving')}}>
-          <Text style={{color: 'white', paddingVertical: 5}}>Make money driving</Text>
+          <Text style={{color: 'white', paddingVertical: 5}}>Lái xe kiếm tiền </Text>
         </Pressable>
 
 
@@ -61,7 +61,7 @@ const CustomDrawer = (props) => {
 
       {/* Make money */}
       <Pressable onPress={() => {dispatch(Logout()) }}>
-        <Text style={{padding: 5, paddingLeft: 20}}>Logout</Text>
+        <Text style={{padding: 5, paddingLeft: 20}}>Đăng xuất</Text>
       </Pressable>
     </DrawerContentScrollView>
   );
